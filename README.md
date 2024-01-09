fa54b44478a1c962c7fe5096b70a812781c58f5c
charlyromytanga <charlyromytanga.cytech@gmail.com>
2024-01-09 19:21:59 +0100

    update files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Projet_C
-AEF
---
+++ Sources/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "Automate.h"
+
+int main(){
+
+    /**************Debut automate*************/
+    // Déclaration des varaibles pour test
+    
+
+
+    // Instructions
+    
+
+    /**************Fin automate*************/
+
+
+    /**************Debut mot****************/
+
+
+
+
+
+    // Déclaration de variables de tests
+
+
+    /**************Fin Mot****************/
+
+
+
+
+    return 0;
+}
